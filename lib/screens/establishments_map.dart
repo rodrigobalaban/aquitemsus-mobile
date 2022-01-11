b1342962693de495892892cd8df5caf101c7b273
Rodrigo Balaban Zbytkowski <rodrigo.balaban@gmail.com>
2022-01-10 21:49:03 -0300

    Add info window on tap on marker

--- lib/screens/establishments_map.dart
+++ lib/screens/establishments_map.dart
@@ -5,9 +5,11 @@ import 'package:aquitemsus/models/location.dart';
 import 'package:aquitemsus/utils/establishment_service.dart';
 import 'package:aquitemsus/utils/location_service.dart';
 import 'package:aquitemsus/widgets/nav_bar.dart';
+import 'package:clippy_flutter/triangle.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:custom_info_window/custom_info_window.dart';
 
 class EstablishmentsMap extends StatefulWidget {
   const EstablishmentsMap({Key? key}) : super(key: key);
@@ -17,6 +19,8 @@ class EstablishmentsMap extends StatefulWidget {
 }
 
 class _EstablishmentMapState extends State<EstablishmentsMap> {
+  final CustomInfoWindowController _infoWindowController =
+      CustomInfoWindowController();
   late List<Establishment> _establishments;
   late String _mapStyle;
   late Location _userLocation;
@@ -28,6 +32,12 @@ class _EstablishmentMapState extends State<EstablishmentsMap> {
     _loadMapStyle();
     _getUserLocation();
     _showEstablishmentsNearby();
+  }
+
+  @override
+  void dispose() {
+    _infoWindowController.dispose();
+    super.dispose();
   }
 
   void _loadMapStyle() {
@@ -43,6 +53,7 @@ class _EstablishmentMapState extends State<EstablishmentsMap> {
 
   void _onMapCreated(GoogleMapController controller) {
     controller.setMapStyle(_mapStyle);
+    _infoWindowController.googleMapController = controller;
   }
 
   _showEstablishmentsNearby() async {
@@ -52,15 +63,82 @@ class _EstablishmentMapState extends State<EstablishmentsMap> {
 
   _setMarkers() async {
     for (var establishment in _establishments) {
-      _markers.add(Marker(
-          markerId: MarkerId(establishment.id.toString()),
-          icon: await BitmapDescriptor.fromAssetImage(
-              ImageConfiguration.empty,
-              EstablishmentService()
-                  .getIconPathByCategory(establishment.category)),
-          position: LatLng(establishment.location.latitude,
-              establishment.location.longitude)));
+      var markerId = MarkerId(establishment.id.toString());
+      var position = LatLng(
+          establishment.location.latitude, establishment.location.longitude);
+
+      var marker = Marker(
+        markerId: markerId,
+        icon: await BitmapDescriptor.fromAssetImage(
+            ImageConfiguration.empty,
+            EstablishmentService()
+                .getIconPathByCategory(establishment.category)),
+        position: position,
+        onTap: () => _showInfoWindow(establishment, position),
+      );
+
+      _markers.add(marker);
     }
+  }
+
+  _showInfoWindow(Establishment establishment, LatLng position) {
+    _infoWindowController.addInfoWindow!(
+      Column(
+        children: [
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(4),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      establishment.category.name,
+                      style: const TextStyle(
+                          color: Color.fromRGBO(84, 133, 229, 1),
+                          fontSize: 10,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      establishment.name,
+                      style: const TextStyle(
+                          color: Colors.black, fontWeight: FontWeight.bold),
+                    ),
+                    const Text(
+                      'Atendimento de Seg. a Sex.',
+                      style: TextStyle(
+                          color: Color.fromRGBO(154, 154, 154, 1), height: 1.5),
+                    ),
+                    const Text(
+                      'Clique para ver mais informações',
+                      style: TextStyle(
+                        color: Colors.black87,
+                        fontSize: 9,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              width: double.infinity,
+              height: double.infinity,
+            ),
+          ),
+          Triangle.isosceles(
+            edge: Edge.BOTTOM,
+            child: Container(
+              color: Colors.white,
+              width: 20.0,
+              height: 10.0,
+            ),
+          ),
+        ],
+      ),
+      position,
+    );
   }
 
   @override
@@ -75,17 +153,29 @@ class _EstablishmentMapState extends State<EstablishmentsMap> {
           future: _getUserLocation(),
           builder: (context, snapshot) {
             if (snapshot.hasData) {
-              return GoogleMap(
-                myLocationEnabled: true,
-                myLocationButtonEnabled: false,
-                initialCameraPosition: CameraPosition(
-                  target:
-                      LatLng(_userLocation.latitude, _userLocation.longitude),
-                  zoom: 15,
+              return Stack(children: [
+                GoogleMap(
+                  myLocationEnabled: true,
+                  myLocationButtonEnabled: false,
+                  initialCameraPosition: CameraPosition(
+                    target:
+                        LatLng(_userLocation.latitude, _userLocation.longitude),
+                    zoom: 15,
+                  ),
+                  onMapCreated: _onMapCreated,
+                  onTap: (position) => _infoWindowController.hideInfoWindow!(),
+                  onCameraMove: (position) {
+                    _infoWindowController.onCameraMove!();
+                  },
+                  markers: _markers,
                 ),
-                onMapCreated: _onMapCreated,
-                markers: _markers,
-              );
+                CustomInfoWindow(
+                  controller: _infoWindowController,
+                  height: 110,
+                  width: 250,
+                  offset: 50,
+                )
+              ]);
             } else {
               return const Text('Carregando localização...');
             }
